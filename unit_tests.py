170e114513b9aadc7d0600c45b03b75cbfe5b48c
himself65 <761282619@qq.com>
2018-05-05 14:26:31 +0800

    fixed bugs

--- unit_tests.py
+++ unit_tests.py
@@ -6,16 +6,12 @@ from luogu import LuoguBrowser, IO
 import ssl
 
 defaultURL = "https://www.luogu.org"
-mySpaceNoticeURL = "https://www.luogu.org//space/ajax_getnotice?"
-myNoticeURL = "https://www.luogu.org/space/ajax_getnotice?uid=72813&mynotice=1&page="
-AcInf = {"uid": "72813", "mynotice": 1, "page": 1}
 
 
 def BrowserDefaultTest():
     """
     单元测试部分
-    测试内容：LuoguBrowser是否可以正常使用，通过向空间通知发送消息
-    需求：将cookie.js文件补全
+    测试内容：LuoguBrowser是否可以正常使用
     """
     userNumber = []
     userUrl = []
@@ -27,17 +23,12 @@ def BrowserDefaultTest():
         AcInf)
     # Init browser
     browser = LuoguBrowser()
-    ## Add Cookie
-    cookie = IO.getJson()['cookie']
-    browser.insert_headers('cookie', cookie)
     ## View Web
-    browser.openURL(messageURL)
+    browser.openURL(defaultURL)
     ## getData
     data = browser.getData()
     data = LuoguBrowser.ungzip(data)
     print(data)
-    if LuoguBrowser.check_Accessible(data):
-        print("Access")
 
 
 def OITest():
