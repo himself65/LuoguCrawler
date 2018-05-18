10e50b03af5b9e3bc8af3fbd6248174192486ed2
himself65 <761282619@qq.com>
2018-05-18 23:57:43 +0800

    还未测试

--- user_info_download.py
+++ user_info_download.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import sqlite3
+import sqlite3  # 数据库
 import gc
-import pika
 import urllib3
-import multiprocessing
-import threading
+import multiprocessing  # 多线程模块
+import threading  # 多线程模块
 import ssl
 import re  # 正则表达式
 
+from time import time
 from queue import Queue
 from bs4 import BeautifulSoup
 from luogu import LuoguBrowser
@@ -23,35 +23,88 @@ from luogu import LuoguBrowser
 """
 __author__ = 'himself65'
 __license__ = 'MIT'
+__email__ = 'himself6565@gmail.com'
 
+# SETTINGS
 DATABASE = {
     'name': 'himself65',
 }
-
+Threads_Number = 10  # 默认10个爬虫列队
 DEBUG = True
+
+# END
+
+GET_TASKS_SQL = '''SELECT ID FROM TASK;'''
+
+
+def get_database():
+    """获取数据库"""
+    name = DATABASE['name'] + '.db'
+    database = sqlite3.connect(name)
+    return database
 
 
 def task_producer(a, b):
     """生成任务 区间[a, b]"""
-    db_name = DATABASE['name']
-    db = sqlite3.connect(db_name)
+    db = get_database()
     cursor = db.cursor()
     for i in range(a, b + 1):
         create_task_sql = '''INSERT INTO TASK(ID)
-                        (%d);''' % (i)
+                        VALUES (%d);''' % (i)
         cursor.executescript(create_task_sql)
     db.close()
 
 
 def get_task(task_que: Queue):
-    """任务队列 TODO"""
-
-    pass
+    """任务队列，从数据库中加载任务放到task_que中"""
+    db = get_database()
+    cursor = db.cursor()
+    cursor.execute(GET_TASKS_SQL)
+    task_list = cursor.fetchall()
+    if len(task_list) != 0:
+        for item in task_que:
+            time_1 = time()
+            task_que.put(item)
+            print("加载任务耗时: %lf" % (time() - time_1))
+    else:
+        print("没有找到任务")
+    db.close()
 
 
 def save_task(save_que: Queue):
     """存储队列 TODO"""
-    pass
+    db = get_database()
+    cursor = db.cursor()
+    while not save_que.empty():
+        item = save_que.get(timeout=5)
+        uid = item['uid']
+        name = item['name']
+        sub_num = item['submit_num']
+        ac_num = item['ac_num']
+        contribute = itme['contribute']
+        active = item['active']
+        integral = item['integral']
+        created_time = item['created_time']
+        save_user_sql = '''INSERT INTO USER(ID, 
+                                            NAME, 
+                                            SUBMIT_NUM,
+                                            AC_NUM, 
+                                            CONTRIBUTE,
+                                            ACTIVE,
+                                            INTEGRAL,
+                                            CREATED_TIME)
+                            VALUES(%d %s %d %d %d %d %d %s);
+                            DELETE FROM TASK WHERE ID = %d;''' % (uid, name,
+                                                                  sub_num,
+                                                                  ac_num,
+                                                                  contribute,
+                                                                  active,
+                                                                  integral,
+                                                                  created_time,
+                                                                  uid)
+        # Save
+        cursor.execute(save_user_sql)
+        save_que.task_done()
 
 
 def crawler(task_que: Queue, save_que: Queue):
@@ -118,16 +171,16 @@ def crawler(task_que: Queue, save_que: Queue):
             created_time = board_items[6].find('span', {
                 'class': 'lg-right'
             }).get_text()
-            luogu_user = [
-                user_name,
-                submit_num,
-                ac_num,
-                contribute,
-                active,
-                integral,
-                created_time,
-            ]
-            print(luogu_user)
+            luogu_user = {
+                'uid': user_id,
+                'name': user_name,
+                'submit_num': submit_num,
+                'ac_num': ac_num,
+                'contribute': contribute,
+                'active': active,
+                'integral': integral,
+                'created_time': created_time,
+            }
         else:
             """隐私代码用户"""
             acts = board_items[1].find('span', {
@@ -140,25 +193,22 @@ def crawler(task_que: Queue, save_que: Queue):
             created_time = board_items[3].find('span', {
                 'class': 'lg-right'
             }).get_text()
-            luogu_user = [
-                user_name,
-                contribute,
-                active,
-                integral,
-                created_time,
-            ]
-            print(luogu_user)
+            luogu_user = {
+                'uid': user_id,
+                'name': user_name,
+                'submit_num': -1,
+                'ac_num': -1,
+                'contribute': contribute,
+                'active': active,
+                'integral': integral,
+                'created_time': created_time,
+            }
         if DEBUG:
-            pass
-            # for i, item in enumerate(board_items):
-            #     print(i, item)
+            print(luogu_user)
 
-
-def get_database():
-    """获取数据库"""
-    name = DATABASE['name'] + '.db'
-    database = sqlite3.connect(name)
-    return database
+        # Finished
+        save_que.put(luogu_user)
+        task_que.task_done()
 
 
 def init():
@@ -174,39 +224,40 @@ def init():
                             ACTIVE             INT,
                             INTEGRAL           INT,
                             CREATED_TIME       TEXT
-                        );
-                        CREATE TABLE IF NOT EXISTS TASK(
+                        );'''
+    cursor.executescript(create_table_sql)
+    create_table_sql = '''CREATE TABLE IF NOT EXISTS TASK(
                             ID INT PRIMARY KEY NOT NULL
                         );'''
-    check_tasks_sql = '''SELECT ID FROM TASK;'''
-    user_list = '''SELECT * FROM USER'''
-    cursor.executescript(create_table_sql)
-    #
-    cursor.executescript(check_tasks_sql)
-    task_list = cursor.fetchall()
-    #
-    cursor.executescript(user_list)
-    if len(task_list) == 0:
-        print("finished!")
-
-    # close database
+    cursor.execute(create_table_sql)
+    # 此处不用execute则无法获取，暂且不知道原因
+    cursor.execute(GET_TASKS_SQL)
+    exist_task = cursor.fetchone()
+    if exist_task is None:
+        print("no task exist!")
+        exit()
+    # Close DataBase
     db.close()
 
 
 def test(tq: Queue, sq: Queue):
+    """Unit Test Part"""
     init()
 
 
 def main(tq: Queue, sq: Queue):
-    threads_list = []
-
-    for td_id, info in enumerate(page_ranges_list):
-        td = threading.Thread(
-            target=crawler, args=(work_que, save_que), name=str(td_id))
+    # Create Theads
+    for td_id in range(1, Threads_Number + 1):
+        td = threading.Thread(target=crawler, args=(tq, sq), name=str(td_id))
         threads_list.append(td)
 
     for td in threads_list.index():
         td.start()
+
+    save_td = threading.Thread(target=save_task, args=(sq), name='Save_Que')
+    save_td.start()
+    save_td.join()
+    print('Finished')
 
 
 if __name__:
@@ -215,4 +266,4 @@ if __name__:
     # 任务列表
     task_que = Queue()
     save_que = Queue()
-    test(task_que, save_que)
+    main(task_que, save_que)
